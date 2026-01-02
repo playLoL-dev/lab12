d34b3113a919b80f92ff8e934691051d0a30c574
playLoL <playza.1987@gmail.com>
2026-01-02 14:24:54 +0700

    lab12_3.cpp completed

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void mySwap(int &,int &); 
 
 int main(){
+
 	int x,y;
 	cin >> x >> y;
 	cout << "Before swapping:\n";
@@ -14,4 +15,10 @@ int main(){
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
+}
+
+void mySwap(int &x,int &y){
+	int temp = x;
+	x = y;
+	y = temp;
 }
